2428d2343c5ad3c28a93ec6c9e402fcd8c6586ff
wunaifu <1278022159@qq.com>
2018-07-03 16:52:15 +0800

    列表展开收缩

---
+++ app/src/main/java/com/wnf/expandablelayoutui/MainActivity.java
@@ -0,0 +1,47 @@
+package com.wnf.expandablelayoutui;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+
+import com.wnf.expandablelayoutui.expandableview.ExpandableLayoutListView;
+
+public class MainActivity extends Activity {
+
+    private final String[] array = {"Hello", "World", "Android", "is", "Awesome", "World",
+            "Android", "is", "Awesome", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.view_row, R.id.header_text, array);
+        final ExpandableLayoutListView expandableLayoutListView = (ExpandableLayoutListView) findViewById(R.id.listview);
+
+        expandableLayoutListView.setAdapter(arrayAdapter);
+    }
+
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+}
